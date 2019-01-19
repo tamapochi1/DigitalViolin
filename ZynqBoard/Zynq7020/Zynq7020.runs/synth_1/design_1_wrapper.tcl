a5f46f305b7ab205296587d07c080f8c38ca49e1
ReijiPochi <koyuki-2009@live.jp>
2019-01-19 23:51:40 +0900

    インプリメンテーションのクリティカルエラーを処理

--- ZynqBoard/Zynq7020/Zynq7020.runs/synth_1/design_1_wrapper.tcl
+++ ZynqBoard/Zynq7020/Zynq7020.runs/synth_1/design_1_wrapper.tcl
@@ -17,7 +17,7 @@ proc create_report { reportName command } {
     send_msg_id runtcl-5 warning "$msg"
   }
 }
-set_msg_config -id {HDL-1065} -limit 10000
+set_param synth.incrementalSynthesisCache C:/Users/Reiji/AppData/Roaming/Xilinx/Vivado/.Xil/Vivado-12832-Reiji-PC/incrSyn
 set_msg_config -id {Synth 8-256} -limit 10000
 set_msg_config -id {Synth 8-638} -limit 10000
 create_project -in_memory -part xc7z020clg484-1
